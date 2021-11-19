7b96e3c13ea6a5e6dfedb62b0af0053c013816ae
axavierj <alexanderjimenez29@gmail.com>
2021-11-19 07:47:56 -0500

    registered a worker

---
+++ js/util.js
@@ -0,0 +1,13 @@
+//register a service worker in the root directory called "sw.js"
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("sw.js")
+    .then((reg) => {
+      // registration worked
+      console.log("Registration succeeded. Scope is " + reg.scope);
+    })
+    .catch((error) => {
+      // registration failed
+      console.log("Registration failed with " + error);
+    });
+}
